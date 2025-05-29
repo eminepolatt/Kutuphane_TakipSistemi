eb5ff202e700bbff14f9cde85bd4f670ba6a837b
FatmaAleyna <ftmalyndgn23@gmail.com>
2025-05-29 22:16:27 +0300

    VeriEkle() metodu çağrıldı.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Kutuphane_Takip_Sistemi
 {
-    internal class Program
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Kutuphane kutuphane = new Kutuphane();
+
+            kutuphane.VeriEkle();
+
+
+            while (true)
+            {
+                Console.WriteLine("\n==== Kütüphane Takip Sistemi ====");
+                Console.WriteLine("1 - Kitap Ekle");
+                Console.WriteLine("2 - Kitapları Listele");
+                Console.WriteLine("3 - Üye Ekle");
+                Console.WriteLine("4 - Üyeleri Listele");
+                Console.WriteLine("5 - Kitap Ödünç Ver");
+                Console.WriteLine("6 - Kitap İade Et");
+                Console.WriteLine("7 - Çıkış");
+                Console.Write("Seçiminiz: ");
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1": kutuphane.KitapEkle(); break;
+                    case "2": kutuphane.KitapListele(); break;
+                    case "3": kutuphane.UyeEkle(); break;
+                    case "4": kutuphane.UyeListele(); break;
+                    case "5": kutuphane.KitapOduncVer(); break;
+                    case "6": kutuphane.KitapIadeEt(); break;
+                    case "7": return;
+                    default: Console.WriteLine("Geçersiz seçim."); break;
+                }
+            }
+        }
     }
 }
