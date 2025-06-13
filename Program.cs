fbed7c26f1c170b3f3f56e91ae6e29eafbefb278
Emine POLAT <162355952+eminepolatt@users.noreply.github.com>
2025-06-13 03:45:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,45 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Kutuphane_TakipSistemi_Form;
+using System;
+using System.Windows.Forms;
+
 
 namespace Kutuphane_Takip_Sistemi
 {
     class Program
     {
-        static void Main(string[] args)
+        
+
+        [STAThread]
+        static void Main()
         {
-            Kutuphane kutuphane = new Kutuphane();
-
-            kutuphane.VeriEkle();
-
-
-            while (true)
-            {
-                Console.WriteLine("\n==== Kütüphane Takip Sistemi ====");
-                Console.WriteLine("1 - Kitap Ekle");
-                Console.WriteLine("2 - Kitapları Listele");
-                Console.WriteLine("3 - Üye Ekle");
-                Console.WriteLine("4 - Üyeleri Listele");
-                Console.WriteLine("5 - Kitap Ödünç Ver");
-                Console.WriteLine("6 - Kitap İade Et");
-                Console.WriteLine("7 - Çıkış");
-                Console.Write("Seçiminiz: ");
-                string secim = Console.ReadLine();
-
-                switch (secim)
-                {
-                    case "1": kutuphane.KitapEkle(); break;
-                    case "2": kutuphane.KitapListele(); break;
-                    case "3": kutuphane.UyeEkle(); break;
-                    case "4": kutuphane.UyeListele(); break;
-                    case "5": kutuphane.KitapOduncVer(); break;
-                    case "6": kutuphane.KitapIadeEt(); break;
-                    case "7": return;
-                    default: Console.WriteLine("Geçersiz seçim."); break;
-                }
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // FORM1'İ ÇALIŞTIR
         }
     }
 }
+
+
